eb619bff519099da617cba7848c5ecf51b3c6881
sanj-git <sanjvasn@gmail.com>
2025-11-19 16:33:08 +0530

    ..

--- routes/dataFetchRoutes.js
+++ routes/dataFetchRoutes.js
@@ -4,11 +4,6 @@ const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
-// Allow OPTIONS for all routes inside /api/*
-router.options("/*", (req, res) => {
-  res.sendStatus(204);
-});
-
 // Protected routes
 router.get("/get-gcs-data", authMiddleware, getGCSData);
 router.post("/upload-csv", authMiddleware, uploadCsvToGcs);
