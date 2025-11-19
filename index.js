e67cad0e2995a26c8cfb9904bb195c417a409392
sanj-git <sanjvasn@gmail.com>
2025-11-19 15:18:17 +0530

    cors options

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dataFetchRoutes = require("./routes/dataFetchRoutes");
 
-const corsOptions = { origin: "*" };
+const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors()); // Enable CORS
+app.options("*", cors()); // Enable preflight everywhere
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.use("/api", dataFetchRoutes);
+app.use("/api/data", dataFetchRoutes);
 
-// Cloud Run requires listening on process.env.PORT
+// Cloud Run uses PORT env variable
 const PORT = process.env.PORT || 8080;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 app.listen(PORT, "0.0.0.0", () => {
-  // Add '0.0.0.0' here
   console.log(`Server running on port ${PORT}`);
 });
--- routes/dataFetchRoutes.js
+++ routes/dataFetchRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const { getGCSData, uploadCsvToGcs } = require("../controllers/DataController");
 const router = express.Router();
-// const {
-//   processAudioPrompt,
-//   processTextPrompt,
-// } = require("../controllers/lexController");
 
-// router.post("/audio-prompt", validateAuthToken, upload.single("audio"), processAudioPrompt);
 router.get("/get-gcs-data", getGCSData);
 router.post("/upload-csv", uploadCsvToGcs);
 
