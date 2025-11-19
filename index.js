dac1cf75348934f08581b306f1871c54ab242e53
sanj-git <sanjvasn@gmail.com>
2025-11-19 17:26:44 +0530

    all cors

--- index.js
+++ index.js
@@ -19,27 +19,27 @@ const PORT = process.env.PORT || 8080;
 // );
 // app.options("/", cors());
 
-const allowedOrigins = [
-  "https://ul-gcp-frontend-react-1076232659917.europe-west3.run.app",
-];
+// const allowedOrigins = [
+//   "https://ul-gcp-frontend-react-1076232659917.europe-west3.run.app",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // Or, if you want to allow requests from any origin (less secure for production but useful for development):
-// app.use(cors());
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.static("public"));
@@ -51,10 +51,10 @@ app.get("/", (req, res) => {
 // Attach API routes
 app.use("/api", dataFetchRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-// app.listen(PORT, "0.0.0.0", () => {
+// app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
