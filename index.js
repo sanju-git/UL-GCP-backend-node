898be1746e109b3eb49be7195b36d0ca630768e2
sanj-git <sanjvasn@gmail.com>
2025-11-19 15:25:03 +0530

    reverted index.js for CORS errors

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const dataFetchRoutes = require("./routes/dataFetchRoutes");
 
-const app = express();
+const corsOptions = { origin: "*" };
 
-app.use(cors()); // Enable CORS
-app.options("*", cors()); // Enable preflight everywhere
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.use("/api/data", dataFetchRoutes);
+app.use("/api", dataFetchRoutes);
 
-// Cloud Run uses PORT env variable
+// Cloud Run requires listening on process.env.PORT
 const PORT = process.env.PORT || 8080;
 
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 app.listen(PORT, "0.0.0.0", () => {
+  // Add '0.0.0.0' here
   console.log(`Server running on port ${PORT}`);
 });
