2237175b64cc79f5710f0972b2fd5ac406713103
sanj-git <sanjvasn@gmail.com>
2025-11-20 11:26:26 +0530

    removed middleware

--- index.js
+++ index.js
@@ -1,60 +1,26 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const dataFetchRoutes = require("./routes/dataFetchRoutes");
 
-const app = express();
-// Cloud Run port binding
-const PORT = process.env.PORT || 8080;
+const corsOptions = { origin: "*" };
 
-// app.use(
-//   cors({
-//     origin: [
-//       "https://ul-gcp-frontend-react-1076232659917.europe-west3.run.app/",
-//       "http://localhost:5173",
-//     ],
-//     methods: ["GET,POST,PUT,DELETE,PATCH"],
-//     allowedHeaders: ["Content-Type,Authorization"],
-//   })
-// );
-// app.options("/", cors());
-
-// const allowedOrigins = [
-//   "https://ul-gcp-frontend-react-1076232659917.europe-west3.run.app",
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const msg =
-//           "The CORS policy for this site does not allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
-
-// Or, if you want to allow requests from any origin (less secure for production but useful for development):
-app.use(cors());
-
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(express.static("public"));
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-// Attach API routes
 app.use("/api", dataFetchRoutes);
+
+// Cloud Run requires listening on process.env.PORT
+const PORT = process.env.PORT || 8080;
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
 
 app.listen(PORT, "0.0.0.0", () => {
+  // Add '0.0.0.0' here
   console.log(`Server running on port ${PORT}`);
 });
--- routes/dataFetchRoutes.js
+++ routes/dataFetchRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { getGCSData, uploadCsvToGcs } = require("../controllers/DataController");
-const authMiddleware = require("../middleware/auth");
+// const authMiddleware = require("../middleware/auth");
 
 // Protected routes
-router.get("/get-gcs-data", authMiddleware, getGCSData);
-router.post("/upload-csv", authMiddleware, uploadCsvToGcs);
+router.get("/get-gcs-data", getGCSData);
+router.post("/upload-csv", uploadCsvToGcs);
 
 module.exports = router;
