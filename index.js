a41d4013a9a61e9e2320624b47153622011406f0
Dharani Chandran <dharanichandran.bc@gmail.com>
2025-12-04 14:54:09 +0530

    port change

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ app.get("/", (req, res) => {
 app.use("/api", dataFetchRoutes);
 // Cloud Run requires listening on process.env.PORT
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, async () => {
-  console.log(`Server running on port ${PORT}`);
-});
-// app.listen(PORT, "0.0.0.0", () => {
-//   // Add '0.0.0.0' here
+// app.listen(PORT, async () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
-
-
-
-require("dotenv").config(); 
+app.listen(PORT, "0.0.0.0", () => {
+  // Add '0.0.0.0' here
+  console.log(`Server running on port ${PORT}`);
+});
