9a4ab82b6622ac780e495d34fc6e456335b60bb2
Dharani Chandran <dharanichandran.bc@gmail.com>
2025-12-04 12:38:45 +0530

    removed databricks

--- routes/dataFetchRoutes.js
+++ routes/dataFetchRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getGCSData, uploadCsvToGcs, getSQLData, getProductByCode, updateProductName, getUCProductMappingData, insertUCProductMappingData} = require("../controllers/DataController");
+const { getGCSData, uploadCsvToGcs, getSQLData, getProductByCode, updateProductName} = require("../controllers/DataController");
 // const authMiddleware = require("../middleware/auth");
 
 // Protected routes
@@ -10,7 +10,7 @@ router.post("/upload-csv", uploadCsvToGcs);
 router.get("/get-sql-data", getSQLData);
 router.get("/get-sql-data/:productcode", getProductByCode); 
 router.put("/get-sql-data/:productcode", updateProductName);  
-router.get("/get-uc-data", getUCProductMappingData);  
-router.post("/insert-uc-data", insertUCProductMappingData);
+// router.get("/get-uc-data", getUCProductMappingData);  
+// router.post("/insert-uc-data", insertUCProductMappingData);
 
 module.exports = router;
