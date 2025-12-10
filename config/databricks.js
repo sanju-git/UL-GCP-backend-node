b6c751077a32d01486f5315ae1ee8739fbd3bfb6
Dharani Chandran <dharanichandran.bc@gmail.com>
2025-12-10 18:56:34 +0530

    databricks pat change

--- config/databricks.js
+++ config/databricks.js
@@ -17,14 +17,15 @@
 
 // module.exports = { getDBXClient };
 
+
 const { GoogleAuth } = require("google-auth-library");
 const databricks = require("@databricks/sql");
 
 async function getGoogleIDToken(audience) {
   const auth = new GoogleAuth();
-  const client = await auth.getIdTokenClient(audience);
-  const headers = await client.getRequestHeaders();
-  return headers["Authorization"].replace("Bearer ", "");
+  const client = await auth.getClient();
+  const token = await client.fetchIdToken(audience);
+  return token; 
 }
 
 let dbxClient = null;
@@ -35,7 +36,7 @@ async function getDBXClient() {
 
     dbxClient = new databricks.DBSQLClient();
     await dbxClient.connect({
-      token: idToken,                               
+      token: idToken,
       host: process.env.DATABRICKS_HOST,
       path: process.env.DATABRICKS_HTTP_PATH,
       port: Number(process.env.DATABRICKS_PORT || 443),
@@ -45,3 +46,35 @@ async function getDBXClient() {
 }
 
 module.exports = { getDBXClient };
+
+
+// const { GoogleAuth } = require("google-auth-library");
+// const databricks = require("@databricks/sql");
+
+// async function getGoogleIDToken(audience) {
+//   const auth = new GoogleAuth();
+//   const client = await auth.getIdTokenClient(audience);
+//   const headers = await client.getRequestHeaders();
+//   console.log(headers)
+//   return headers["authorization"].replace("Bearer ", "");
+// }
+
+// let dbxClient = null;
+
+// async function getDBXClient() {
+//   if (!dbxClient) {
+//     const idToken = await getGoogleIDToken(process.env.DATABRICKS_HOST);
+//     console.log(idToken)
+
+//     dbxClient = new databricks.DBSQLClient();
+//     await dbxClient.connect({
+//       token: idToken,                               
+//       host: process.env.DATABRICKS_HOST,
+//       path: process.env.DATABRICKS_HTTP_PATH,
+//       port: Number(process.env.DATABRICKS_PORT || 443),
+//     });
+//   }
+//   return dbxClient;
+// }
+
+// module.exports = { getDBXClient };
