6e3d9800cc1f2bba311b98f2eb483484e92880be
Dharani Chandran <dharanichandran.bc@gmail.com>
2025-12-15 12:30:24 +0530

    sql variable changes

--- config/db.js
+++ config/db.js
@@ -1,43 +1,3 @@
-// // const mysql = require("mysql2/promise");
-// // const DB_HOST = "34.39.62.79";
-// // const DB_NAME = "tg-sqlserver-dev-02";
-// // const DB_USER = "ul-test-01";;
-// // const DB_PASS = "GCPass3ssment";
-
-// // let pool;
-
-// // const getPool = () => {
-// //   if (pool) return pool;
-
-// //   pool = mysql.createPool({
-// //     host: DB_HOST,
-// //     user: DB_USER,
-// //     password: DB_PASS,
-// //     database: DB_NAME,
-// //     waitForConnections: true,
-// //     connectionLimit: 10,
-// //     queueLimit: 0,
-// //   });
-
-// //   console.log("Direct MySQL Connection Pool Created (No IAM)");
-// //   return pool;
-// // };
-
-// // async function testConnection() {
-// //   try {
-// //     const pool = getPool();
-// //     const [rows] = await pool.query("SELECT 1 + 1 AS result");
-// //     console.log("MySQL Connected Successfully! Test Result:", rows[0].result);
-// //   } catch (error) {
-// //     console.error("MySQL Connection Failed:", error.message);
-// //   }
-// // }
-
-// // testConnection();
-
-// // module.exports = { getPool };
-
-
 const sql = require("mssql");
 
 let pool;
@@ -45,11 +5,11 @@ let pool;
 async function getPool() {
   if (!pool) {
     pool = await sql.connect({
-      user:'Demo',
-      password:'GCPass3ssment',
-      server:'10.154.0.3',
-      database:'Demo',
-      port:1433,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      server:process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port:process.env.DB_PORT,
       options: {
         trustServerCertificate: true,
         encrypt: true
@@ -62,30 +22,4 @@ async function getPool() {
 module.exports = { getPool };
 
 
-// const { Connector } = require("@google-cloud/cloud-sql-connector");
-// const sql = require("mssql");
-
-// let pool;
-
-// async function getPool() {
-//   if (!pool) {
-//     const connector = new Connector();
-
-//     const options = await connector.getTediousOptions({
-//       instanceConnectionName: "gcp-data-ai-architecture:europe-west2:tg-sqlserver-dev-02",
-//       ipType: "PRIVATE",  // <── connect via private IP through secure tunnel
-//     });
-
-//     pool = await sql.connect({
-//       ...options,
-//       user: "ul-test-01",
-//       password: "GCPass3ssment",
-//       database: "demo",
-//       options: { trustServerCertificate: true }
-//     });
-//   }
-//   return pool;
-// }
-
-// module.exports = { getPool };
 
